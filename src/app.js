31ec6b642ae13106fa23441875acc872e05817cb
itsmechrissyj <mrscjones1985@gmail.com>
2023-02-10 14:26:41 +0000

    Add JS

---
+++ src/app.js
@@ -0,0 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+}
+
+let apiKey = "2b605afdd2dbaf15fc530f7679deead1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
