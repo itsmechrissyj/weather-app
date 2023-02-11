f02e07febbd91fb28dda2ce61063e1c85fcee5d3
itsmechrissyj <mrscjones1985@gmail.com>
2023-02-11 14:46:59 +0000

    added JS for wind and humidity

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidElement = document.querySelector("#humid");
+  let windyElement = document.querySelector("#windy");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
+  humidElement.innerHTML = response.data.main.humidity;
+  windyElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "2b605afdd2dbaf15fc530f7679deead1";
