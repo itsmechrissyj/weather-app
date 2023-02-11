976d11574103caa81e180e07f29cd11a631c75c7
itsmechrissyj <mrscjones1985@gmail.com>
2023-02-11 16:37:08 +0000

    Icon JS

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ function displayTemperature(response) {
   let humidElement = document.querySelector("#humid");
   let windyElement = document.querySelector("#windy");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -37,10 +38,14 @@ function displayTemperature(response) {
   humidElement.innerHTML = response.data.main.humidity;
   windyElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "2b605afdd2dbaf15fc530f7679deead1";
-let city = "Paris";
+let city = "london";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
