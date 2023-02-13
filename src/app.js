aa3c2edd5651440191a040e975687daa3c4a4e13
itsmechrissyj <mrscjones1985@gmail.com>
2023-02-13 21:20:34 +0000

    Updated CSS

--- src/app.js
+++ src/app.js
@@ -50,12 +50,12 @@ function displayForecast(response) {
             alt=""
             width="42">
             <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max"> ${Math.round(
-              forecastDay.temp.max
-            )}°</span> 
-            <span class="weather-forecast-temperature-min"> ${Math.round(
-              forecastDay.temp.min
-            )}°</span>
+             <span class="weather-forecast-temperature-max">Max: ${Math.round(
+               forecastDay.temp.max
+             )}°C</span> 
+             <span class="weather-forecast-temperature-min">Min: ${Math.round(
+               forecastDay.temp.min
+             )}°C</span>
 </div>
           </div>
         `;
