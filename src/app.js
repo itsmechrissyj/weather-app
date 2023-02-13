6a7f2f2339c5a77b00c3e443d9831092097fb48a
itsmechrissyj <mrscjones1985@gmail.com>
2023-02-13 19:29:03 +0000

    Add Forward Forecast

--- src/app.js
+++ src/app.js
@@ -23,6 +23,33 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon", "Tues"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+            <div class="weather-forecast-date">
+             ${day}
+            </div>
+            <img src="http://openweathermap.org/img/wn/10d@2x.png"
+            alt=""
+            width="56">
+            <div class="weather-forecast-temperature">
+            <span class="weather-forecast-temperature-max"> 18°</span> 
+            <span class="weather-forecast-temperature-min"> 12°</span>
+</div>
+          </div>
+        `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -85,3 +112,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("New York");
+displayForecast();
